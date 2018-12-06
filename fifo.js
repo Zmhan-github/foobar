bcdc5545655ef23f95ea21de1591fad378bcffc8
Marat <marat@deex.exchange>
2018-12-06 16:13:52 +0500

    add fifo algorithm

--- fifo.js
+++ fifo.js
@@ -1,1 +1,27 @@
-console.log("fifo")
+// Queues [кью] Очередь
+// FIFO
+
+function createQueue() {
+  const queue = []
+  return {
+    enqueue(item) {
+      queue.unshift(item)
+    },
+    dequeue() {
+      queue.pop()
+    },
+    peek(){
+      return queue[queue.length - 1]
+    },
+    get length() {
+      return queue.length
+    },
+    isEmpty() {
+      return queue.length === 0
+    }
+  }
+}
+
+
+const q = createQueue()
+console.log(q.length)
